98a9c3f9d7561d0551a7e0b20c0bc84517d81258
Lawrence-at-alx <software.engineer.lawsom234@gmail.com>
2022-10-13 15:14:13 -0700

    My fifth commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,8 +7,7 @@ vim 5-printf.c
  * Return: always 0 (success)
  */
 int main(void)
-{
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
+{ 
+	printf("with proper grammar, but the outcome is a peice of art,\n");
+	return (0);
 }
