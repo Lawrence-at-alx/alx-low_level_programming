d5fee4ea0c8e7f033c2a24410edd0ccf9954a52b
Lawrence-at-alx <software.engineer.lawsom234@gmail.com>
2022-10-13 12:19:58 -0700

    My fourth task commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/*
+ * main- serve as an entry point to the program
+ *Return: By default returns zero if no errors otherwise returns a  non-zero value.
+ */
+int main(void)
+{
+	put("\"Programming is like building a multilingal puzzle");
+	return (0);
+}
