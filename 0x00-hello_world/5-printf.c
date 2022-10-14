384b60d033d4c839055783534c427d86f8c1ee42
Lawrence-at-alx <software.engineer.lawsom234@gmail.com>
2022-10-14 05:02:21 -0700

    My fifth task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
-vim 5-printf.c 
-#include <stdio.h>
-
+ #include <stdio.h>
 /**
  *main - print a test.
  *
  * Return: always 0 (success)
  */
 int main(void)
-{ 
-	printf("with proper grammar, but the outcome is a peice of art,\n");
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
