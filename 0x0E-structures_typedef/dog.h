ec404788efd918cf23bb5ba0f3ce3737ec2577d5
Lawrence-at-alx <software.engineer.lawsom234@gmail.com>
2022-11-14 22:02:59 -0800

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,10 +7,10 @@
  * @age: The age of the dog.
  * @owner: The owner of the dog.
  *
- * Description: The attribute of th dog
+ * Description: The attributes of the dog
  */
 
-struct dog 
+struct dog
 {
 	char *name;
 	float age;
